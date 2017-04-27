311ca7154beeceda32323b061025d160284d736a
Saïd Mezhoud <said.mezhoud@voidmx.net>
2017-04-27 17:01:03 +0200

    init project

---
+++ client/server.js
@@ -0,0 +1,10 @@
+'use strict'
+/* jshint node: true */
+//*********************************************************//
+//    Name: P4
+//    Author: said mezhoud
+//    GitHub: https://github.com/Waregalias
+//    Description: Puissance 4 Application
+//    version: .alpha 0.1.0
+//*********************************************************//
+// app include
