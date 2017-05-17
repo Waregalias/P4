4e44c0f3fce585524e39c30573723252abc69d42
Saïd Mezhoud <said.mezhoud@voidmx.net>
2017-05-17 03:06:21 +0200

    blocked on colonne control function()

--- routes/game.js
+++ routes/game.js
@@ -28,7 +28,6 @@ function ligne(action) {
 
   for(let x=0; x < grille.length; x++) {
     for(let y=0; y < grille[x].length; y++) {
-      //console.log(x,y);
       if(grille[x][y] !== '') {
         switch(grille[x][y]) {
           case 'yellow':
@@ -41,10 +40,8 @@ function ligne(action) {
             break;
         }
         if(yellow === 4) {
-          console.log('yellow win');
 					winner = 'yellow';
         } else if(red === 4) {
-          console.log('red win');
 					winner = 'red';
         }
       } else {
@@ -56,38 +53,43 @@ function ligne(action) {
 	return winner;
 } module.exports.ligne = ligne;
 
-// function colonne(action) {
-//   let yellow = 0;
-//   let red = 0;
-//   let grille = JSON.parse(action);
-//
-//   for(let y=0; y < grille.length; y++) {
-//     for(let x=0; x < grille[y].length; x++) {
-//       console.log(y,x);
-//       if(grille[y][x] !== '') {
-//         switch(grille[y][x]) {
-//           case 'yellow':
-//             yellow++;
-//             //red = 0;
-//             break;
-//           case 'red':
-//             red++;
-//             //yellow = 0;
-//             break;
-//         }
-//         console.log('y&r', yellow, red);
-//         if(yellow === 4) {
-//           console.log('yellow win');
-//         } else if(red === 4) {
-//           console.log('red win');
-//         }
-//       } else {
-//         yellow = 0;
-//         red = 0;
-//       }
-//     }
-//   }
-// } module.exports.colonne = colonne;
+function colonne(action) {
+	let winner = '';
+  let yellow = 0;
+  let red = 0;
+  let grille = JSON.parse(action);
+
+	console.log(grille[0].length);
+  for(let x=grille.length-1; x >= 0; x--) { // 6
+    for(let y=0; y < grille[x].length-1; y++) { // 7
+			console.log(x,y);
+      //console.log(grille[x][y]);
+      if(grille[x][y] !== '') {
+        switch(grille[x][y]) {
+          case 'yellow':
+            yellow++;
+            red = 0;
+            break;
+          case 'red':
+            red++;
+            yellow = 0;
+            break;
+        }
+        console.log('y&r', yellow, red);
+        if(yellow === 4) {
+          console.log('yellow win');
+        } else if(red === 4) {
+          console.log('red win');
+        }
+      } else {
+        yellow = 0;
+        red = 0;
+      }
+    }
+		console.log("--");
+  }
+	return winner;
+} module.exports.colonne = colonne;
 
 function diagonale(action, coord) {
 	let winner = '';
